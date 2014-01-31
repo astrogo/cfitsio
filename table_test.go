c47fc29ab5feb956147fe51d352179333d77ce83
Sebastien Binet <binet@cern.ch>
2014-01-31 18:02:45 +0100

    test: add Table test

---
+++ table_test.go
@@ -0,0 +1,31 @@
+package cfitsio
+
+import "testing"
+
+func TestTable(t *testing.T) {
+	for _, table := range g_tables {
+		fname := table.fname
+		f, err := Open(fname, ReadOnly)
+		if err != nil {
+			t.Fatalf("error opening file [%v]: %v", fname, err)
+		}
+
+		for i := range f.HDUs() {
+			hdu, ok := f.HDUs()[i].(*TableHDU)
+			if !ok {
+				continue
+			}
+			for irow := int64(0); irow < hdu.NumRows(); irow++ {
+				err := hdu.ReadRow(irow)
+				if err != nil {
+					t.Fatalf(
+						"error reading row [%v] (fname=%v, table=%v): %v",
+						irow, fname, hdu.Name(), err,
+					)
+				}
+			}
+		}
+	}
+}
+
+// EOF

fd4b9063102c9dbc18a6c488c12ccc3d06a9bd70
Sebastien Binet <binet@cern.ch>
2014-02-14 14:10:11 +0100

    rows: add scan into struct and map

--- rows.go
+++ rows.go
@@ -2,6 +2,7 @@ package cfitsio
 
 import (
 	"fmt"
+	"reflect"
 )
 
 // Rows is the result of a query on a FITS Table.
@@ -53,17 +54,26 @@ func (rows *Rows) Scan(args ...interface{}) error {
 		rows.err = err
 	}()
 
-	// special case: read everything into the cols.
-	if len(args) == 0 {
-		for _, icol := range rows.cols {
-			err = rows.table.cols[icol].read(rows.table.f, icol, rows.irow)
-			if err != nil {
-				return err
-			}
+	switch len(args) {
+	case 0:
+		// special case: read everything into the cols.
+		return rows.scanAll()
+	case 1:
+		// maybe special case: map? struct?
+		rt := reflect.TypeOf(args[0])
+		switch rt.Kind() {
+		case reflect.Map:
+			return rows.scanMap(args[0].(map[string]interface{}))
+		case reflect.Struct:
+			return rows.scanStruct(args[0])
 		}
-		return err
 	}
 
+	return rows.scan(args...)
+}
+
+func (rows *Rows) scan(args ...interface{}) error {
+	var err error
 	if len(args) != len(rows.cols) {
 		return fmt.Errorf(
 			"cfitsio.Rows.Scan: invalid number of arguments (got %d. expected %d)",
@@ -78,6 +88,47 @@ func (rows *Rows) Scan(args ...interface{}) error {
 		}
 		args[i] = rows.table.cols[icol].Value
 	}
+	return err
+}
+
+func (rows *Rows) scanAll() error {
+	var err error
+	for _, icol := range rows.cols {
+		err = rows.table.cols[icol].read(rows.table.f, icol, rows.irow)
+		if err != nil {
+			return err
+		}
+	}
+	return err
+}
+
+func (rows *Rows) scanMap(data map[string]interface{}) error {
+	var err error
+
+	icols := make([]int, 0, len(data))
+	for k := range data {
+		for _, icol := range rows.cols {
+			if rows.table.cols[icol].Name == k {
+				icols = append(icols, icol)
+				break
+			}
+		}
+	}
+	for _, icol := range icols {
+		col := rows.table.Col(icol)
+		err = rows.table.cols[icol].read(rows.table.f, icol, rows.irow)
+		if err != nil {
+			return err
+		}
+		data[col.Name] = rows.table.cols[icol].Value
+	}
+
+	return err
+}
+
+func (rows *Rows) scanStruct(data interface{}) error {
+	var err error
+	panic("not implemented")
 	return err
 }
 

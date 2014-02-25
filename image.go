3463b258c4e9ab1d38da1f8617a4db239eb213e6
Sebastien Binet <binet@cern.ch>
2014-02-25 18:01:40 +0100

    image: Write image

--- image.go
+++ image.go
@@ -117,6 +117,73 @@ func (hdu *ImageHDU) load(v reflect.Value) error {
 	return err
 }
 
+// Write writes the image to disk
+func (hdu *ImageHDU) Write(data interface{}) error {
+	var err error
+	rv := reflect.ValueOf(data).Elem()
+	if !rv.CanAddr() {
+		return fmt.Errorf("%T is not addressable", data)
+	}
+
+	hdr := hdu.Header()
+	naxes := len(hdr.Axes())
+	if naxes == 0 {
+		return nil
+	}
+	nelmts := 1
+	for _, dim := range hdr.Axes() {
+		nelmts *= int(dim)
+	}
+
+	c_start := C.LONGLONG(0)
+	c_nelmts := C.LONGLONG(nelmts)
+	c_status := C.int(0)
+	c_imgtype := C.int(0)
+	var c_ptr unsafe.Pointer
+
+	switch rv.Interface().(type) {
+	case []byte:
+		c_imgtype = C.TBYTE
+		data := rv.Interface().([]byte)
+		c_ptr = unsafe.Pointer(&data[0])
+
+	case []int16:
+		c_imgtype = C.TSHORT
+		data := rv.Interface().([]int16)
+		c_ptr = unsafe.Pointer(&data[0])
+
+	case []int32:
+		c_imgtype = C.TINT
+		data := rv.Interface().([]int32)
+		c_ptr = unsafe.Pointer(&data[0])
+
+	case []int64:
+		c_imgtype = C.TLONGLONG
+		data := rv.Interface().([]int64)
+		c_ptr = unsafe.Pointer(&data[0])
+
+	case []float32:
+		c_imgtype = C.TFLOAT
+		data := rv.Interface().([]float32)
+		c_ptr = unsafe.Pointer(&data[0])
+
+	case []float64:
+		c_imgtype = C.TDOUBLE
+		data := rv.Interface().([]float64)
+		c_ptr = unsafe.Pointer(&data[0])
+
+	default:
+		panic(fmt.Errorf("invalid image type [%T]", rv.Interface()))
+	}
+
+	C.fits_write_img(hdu.f.c, c_imgtype, c_start+1, c_nelmts, c_ptr, &c_status)
+	if c_status > 0 {
+		return to_err(c_status)
+	}
+
+	return err
+}
+
 func newImageHDU(f *File, hdr Header, i int) (hdu HDU, err error) {
 	switch i {
 	case 0:
